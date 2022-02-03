5cd846a14b14b97dbd669d2be3a8e7a5466dee88
Ralph Smith <ralph@ralphsmithphoto.com>
2022-02-02 22:33:14 -0600

    Added functions to get-int-from-char and to get-char-from-int

--- decryption/alphabet.py
+++ decryption/alphabet.py
@@ -5,7 +5,8 @@ The module for storing the alphabet
 # *** Define any change to the alphabet here ***
 _ALPHABET = " abcdefghijklmnopqrstuvwxyz"  # the leading space should be there
 _ALPHABET_SIZE = len(_ALPHABET)
-_LETTER_POS_DICT = {char: i for i, char in enumerate(_ALPHABET)}
+_LETTER_TO_POS_DICT = {char: i for i, char in enumerate(_ALPHABET)}
+
 
 def get_alphabet():
     """
@@ -22,13 +23,30 @@ def get_alphabet_size():
     return _ALPHABET_SIZE
 
 
-def get_alphabet_char_to_int_dict():
+def get_int_from_char(a_char):
     """
-    Returns a dictionary
-    Keys : a char from the alphabet
-    Values : the index position of the char in the alphabet
+    input: a single char
+    output: an int between 0 and the size of the alphabet
     """
-    return _LETTER_POS_DICT
+    if not isinstance(a_char, str):
+        raise TypeError(f"Please pass a lower case char, you passed a {type(a_char)}")
+    if len(a_char) != 1:
+        raise ValueError(f"Please pass a single character, you passed {len(a_char)} chars")
+    return _LETTER_TO_POS_DICT[a_char]
+
+
+def get_char_from_int(a_int):
+    """
+    input: an int in the range (0, _ALPHABET_SIZE - 1)
+    output: the character from the alphabet at position <a_int>
+
+    """
+    if not isinstance(a_int, int):
+        raise TypeError(f"Please pass an int, you passed a {type(a_int)}")
+    if a_int < 0 or a_int >= _ALPHABET_SIZE:
+        raise ValueError(f"Int out of range. It should be in the range \
+                            (0, {_ALPHABET_SIZE - 1})  You passed: {a_int}")
+    return _ALPHABET[a_int]
 
 
 def main():
@@ -37,11 +55,11 @@ def main():
     """
     # Prints out some info if called from the command line
     print(f"The alphabet is: \'{get_alphabet()}\'")
-    print(f"\nIt contains {get_alphabet_size()} characters.")
-    letter_map = get_alphabet_char_to_int_dict()
-    print("\nThe char to index position mappings are:")
-    for entry in letter_map:
-        print(f"{entry} : {letter_map[entry]}")
+    print(f"It contains {get_alphabet_size()} characters.")
+
+    # test for errors
+    assert get_int_from_char("a") == 1
+    assert get_char_from_int(22) == "v"
 
 if __name__ == "__main__":
     main()
