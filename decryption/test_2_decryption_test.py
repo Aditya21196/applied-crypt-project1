d6e321eafd845a7214307dee55e134a0f2a6cd9f
Ralph Smith <ralph@ralphsmithphoto.com>
2022-03-10 21:59:46 -0600

    working on test 2

--- decryption/test_2_decryption_test.py
+++ decryption/test_2_decryption_test.py
@@ -71,6 +71,10 @@ def get_possible_word_list(words_list, cipher_words):
     output: a list of possible dictionary words that are in the cipherwords
             returns an empyt list if none found
     """
+    if DEBUG:
+        print(f"in get_possible_word_list")
+        print(f"words_list\n{words_list}")
+        print(f"cipher_words len: {len(cipher_words)}\n{cipher_words}")
     plaintext = []
     cipher_word_idx = 0
 
@@ -89,6 +93,8 @@ def get_possible_word_list(words_list, cipher_words):
 
             current_word = find_word(words_list, current_cipher_word)
             if len(current_word) != 0:
+                if DEBUG:
+                    print(f"Current_word_found -> {current_word}\n")
                 plaintext.append(current_word)
                 cipher_word_idx = i - 1
                 break
@@ -159,16 +165,33 @@ def gen_all_permutation_keys(key_space):
     return candidates
 
 
+
 def process_candidate_components(dictionary, component_list, ciphertext_words):
 
     num_candidate_options = [len(entry) for entry in component_list]
+    candidate_lengths = []
+    for entry in component_list:
+        if isinstance(entry, str):
+            candidate_lengths.append(len(entry))
+        else:
+            min = len(entry[0])
+            for sub_entry in entry:
+                if len(sub_entry) < min:
+                    min = len(sub_entry)
+            candidate_lengths.append(min)
+
+    shortest_possible = sum(candidate_lengths) + len(candidate_lengths) - 1
+
 
     if DEBUG:
         print(f"num_candidate_options {num_candidate_options}")
+        print(f"min_candidate_lengths {candidate_lengths}")
+        print(f"Total key space = {math.prod(num_candidate_options)}")
+        print(f"shortest_possible {shortest_possible}")
 
-    print("key gen_all_permutations started")
+    #print("key gen_all_permutations started")
     all_keys = gen_all_permutation_keys(num_candidate_options)  # fix to be a generator
-    print("key gen_all_permutations finished")
+    #print("key gen_all_permutations finished")
 
     if DEBUG:
         print(f"num_candidate_options {num_candidate_options}")
@@ -190,7 +213,11 @@ def process_candidate_components(dictionary, component_list, ciphertext_words):
             return current_candidate
         else:   # text < 500
             chars_to_add = 500 - len(current_candidate)
+            if DEBUG:
+                print(f" -- Before pad_ending")
             padding = pad_ending(chars_to_add, dictionary, ciphertext_words)
+            if DEBUG:
+                print(f" -- After pad ending- padding {padding}")
             current_candidate += padding
             if len(current_candidate) == 500:
                 return current_candidate
@@ -204,11 +231,25 @@ def key_saturated(n, current_vals, max_vals):
             return False
     return True
 
+
 def pad_ending(num_chars, dictionary, cipher_words):
-    print(f"cipher words {cipher_words}")
+    """
+    input   num_chars -> number needed for padding
+            dictionary -> words to find
+            cipher_words -> words to match
+
+    Output  a valid padding string if one is found
+    """
     last_char = cipher_words[-1][-1]
     padded = " " #keep as an empty space
     dict_word_len = num_chars - 1
+
+    if DEBUG:
+        print(f"pad_ending cipher words\n{cipher_words}")
+        print(f"dictionary\n{dictionary}")
+        print(f"last_char {last_char}")
+        print(f"num_chars {num_chars}")
+        print(f"truncated dict_word_len {dict_word_len}")
 
     truncated_dict = [word[:dict_word_len] for word in dictionary if len(word) >= dict_word_len and word[dict_word_len-1] == last_char]
     if len(truncated_dict) == 0:
@@ -226,14 +267,20 @@ def pad_ending(num_chars, dictionary, cipher_words):
         print(f" 225last_words {last_word}")
 
 
-    while len(last_word) == 0:
+    while len(last_word) == 0: #and j > len(cipher_words) - 10:
+        # TODO -> What is the best value for the second conditional
+        #  It says that only look at the last 1/8 of the ciphertext for the padding
+
         j -= 1
-        last_word = get_possible_word_list(truncated_dict, cipher_words[j])
+        last_word = get_possible_word_list(truncated_dict, cipher_words[j:])
 
     if DEBUG:
         print(f"last_words {last_word}")
         print(f"truncated dict: {truncated_dict}")
         print(f"j word: {cipher_words[j:]}")
+
+    if len(last_word) == 0:
+        return padded
 
     padded += last_word[0][random.randint(0,len(last_word[0])-1)]
 
@@ -261,7 +308,16 @@ def find_plaintext(words, ciphertext):
     candidate_text_components = get_candidate_components(plaintext_pieces)
     if DEBUG:
         print(f"\ncandidate_text_components \n{candidate_text_components}\n")
-    return process_candidate_components(words, candidate_text_components, cipher_words)
+    solution = process_candidate_components(words, candidate_text_components, cipher_words)
+
+    print(f"\nspace char '{space}'")
+
+    #todo
+        # if solution totally fails it returns none
+        # make a do while loop
+        # if solution fails, move on to next possible space value and recompute
+
+    return solution
 
 
 def stress_test(low_p, high_p, step, num_repeats, dict):
@@ -269,34 +325,39 @@ def stress_test(low_p, high_p, step, num_repeats, dict):
     for p in range(low_p, high_p, step):
         prob = p / 100
         for j in range(num_repeats):
-            print(f"Current Iteration p {p} j{j}")
+            print(f"\n\nCurrent Iteration p {p} j {j} seed {seed}")
             plaintext = make_a_dict_2_plaintext(dict, seed)
             ciphertext = encrypt.encrypt(plaintext, encrypt.BLANK_KEY, prob, seed)
             cleaned_ciphertext = preprocess.remove_duplicate_char_triplets(ciphertext)
             plaintext_guess = find_plaintext(dict, cleaned_ciphertext)
+            print(f"Plaintext\n{plaintext}\n")
+            print(f"plaintext_guess\n{plaintext_guess}\n")
+
             try:
                 assert len(plaintext_guess) == 500
             except AssertionError as e:
-                print(f"AssertionError - error caused with p {p} j{j}")
+                print(f" *** AssertionError - error caused with p {p} j {j} seed {seed}")
                 raise e
+            except TypeError as e:
+                print(f" *** Type error wile at caused with p {p} j{j} seed {seed}")
             seed += 1
 
 
 
 def main():
     dict2 = load_dictionary()
-    stress_test(10,40,5, 10, dict2)
+    stress_test(low_p = 70,high_p = 76, step = 1, num_repeats = 10, dict = dict2)
 
 
     """
-    seed = 500
+    seed = 37
 
     # Generate Test Data
     plaintext = make_a_dict_2_plaintext(dict2, seed)
     print(f"dict plaintext with seed({seed}):\n{plaintext}\n")
 
     # Encrypt Test Data
-    ciphertext = encrypt.encrypt(plaintext, encrypt.BLANK_KEY, .1, seed)
+    ciphertext = encrypt.encrypt(plaintext, encrypt.BLANK_KEY, .76, seed)
     print(f"ciphertext with seed({seed}) of length {len(ciphertext)}:\n{ciphertext}\n")
 
     # Try to Generate Plaintext
@@ -313,6 +374,7 @@ def main():
 
     #print(is_word_in_ciphertext("moponwsetk","moponwsetk"))
     """
+
 
 if __name__ == "__main__":
     main()
