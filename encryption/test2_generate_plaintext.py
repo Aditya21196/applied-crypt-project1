3954e265da451bbd8d41a4a80bb625aec93c88fe
Qilei Cai <36024574+qc542@users.noreply.github.com>
2022-03-12 19:15:44 -0500

    Updated function that generates Test 2 plaintext

--- encryption/test2_generate_plaintext.py
+++ encryption/test2_generate_plaintext.py
@@ -7,14 +7,25 @@ def get_plaintext():
         words = [w.rstrip() for w in f]
         # Removes the newline character at the end of the string 
         # before appending it to the list
+        
+    char_cnt = 0
+    txt = []
+    
+    while char_cnt < 500:
+        i = np.random.randint(0, len(words) - 1)
+        txt.append(words[i])
+        char_cnt += len(words[i])
 
-    np.random.shuffle(words)
-    plaintext = " ".join(words)
+    return " ".join(txt)[:500]
 
-    return plaintext
+t = get_plaintext()
+print(t)
+print(len(t))
 
+"""
 if __name__ == "__main__":
     # If the script is run directly, save the plaintext as a new file
 
     with open("plaintext_test2.txt", "w") as f:
         f.write(get_plaintext())
+"""
