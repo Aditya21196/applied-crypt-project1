c9ce5199562e580bd8a89838e0a99dcfecb2ab5e
Qilei Cai <36024574+qc542@users.noreply.github.com>
2022-03-13 12:58:12 -0400

    Added function that returns subsequence

--- decryption/find_similar_words.py
+++ decryption/find_similar_words.py
@@ -16,11 +16,32 @@ def count_common_letters(w1, w2):
             
     return dp[-1][-1]
 
-w1 = "fixndy siamilabr worcds"
-w2 = "fitndj simmilar qworvdsp"
+def get_longest_common_subsequence(w1, w2):
+    dp = [[0 for _ in range(len(w1) + 1)] for _ in range(len(w2) + 1)]
+    
+    for i in range(1, len(w2) + 1):
+        for j in range(1, len(w1) + 1):
+            c1, c2 = w2[i - 1], w1[j - 1]
+            x = int(c1 == c2)
+            dp[i][j] = max(dp[i][j - 1], dp[i - 1][j - 1] + x, dp[i - 1][j])
+            
+    res = []
+    for i in range(1, len(dp[-1])):
+        if dp[-1][i] > dp[-1][i - 1]:
+            res.append(w1[i - 1])
+            
+    return "".join(res)
+
+#w1 = "xxxxxxaxxxxbxxxxxc"
+#w2 = "abc"
+w1 = "fxxxxindxxx sixxxxmilarxxx worrrrrds"
+w2 = "find similar words"
 
 res = count_common_letters(w1, w2)
 print("The 1st string: " + w1)
 print("The 2nd string: " + w2)
 print("The length of the longest common subsequence " + 
       "they share is: " + str(res))
+
+seq = get_longest_common_subsequence(w1, w2)
+print("The longest common subsequence is: " + seq)
